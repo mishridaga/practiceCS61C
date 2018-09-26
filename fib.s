c813ca39bb78386f43e83d9c66a1e361f131b7b5
MISHRI DAGA <mishridaga@berkeley.edu>
2018-09-26 12:51:00 -0700

    fixed fib.s

--- fib.s
+++ fib.s
@@ -36,35 +36,5 @@ fib:
 	addi	sp,sp,32
 	jr	ra
 	.size	fib, .-fib
-	.section	.rodata
-	.align	2
-.LC0:
-	.string	"%d"
-	.text
-	.align	1
-	.globl	main
-	.type	main, @function
-main:
-	addi	sp,sp,-32
-	sw	ra,28(sp)
-	sw	s0,24(sp)
-	addi	s0,sp,32
-	li	a5,9
-	sw	a5,-20(s0)
-	lw	a0,-20(s0)
-	call	fib
-	mv	a5,a0
-	mv	a1,a5
-	lui	a5,%hi(.LC0)
-	addi	a0,a5,%lo(.LC0)
-	call	printf
-	call	getchar
-	li	a5,0
-	mv	a0,a5
-	lw	ra,28(sp)
-	lw	s0,24(sp)
-	addi	sp,sp,32
-	jr	ra
-	.size	main, .-main
 	.ident	"GCC: (GNU) 8.2.0"
 	.section	.note.GNU-stack,"",@progbits
